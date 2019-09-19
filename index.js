55769eda3af7369f4051ad1bd0800e88a26655a4
Fernando Matzdorf <fernando@rindecuentas.org>
2019-09-19 11:55:02 -0500

    Added -f flag to specify field to use for uniqueness. Reworked output section to avoid incomplete output when running from Poppins.

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const commandLineArgs = require('command-line-args');
 
 const optionDefinitions = [
   { name: 'collections', alias: 'c', type: String, multiple: true, defaultOption: true },
-  { name: 'database', alias: 'd', type: String }
+  { name: 'database', alias: 'd', type: String },
+  { name: 'field', alias: 'f', type: String }
 ];
 
 const args = commandLineArgs(optionDefinitions);
@@ -13,6 +14,7 @@ if(!args.database || !args.collections) {
     console.log('ERROR: missing parameters.');
     process.exit(1);
 }
+const idField = (args.field)? args.field : 'ocid';
 
 const ocid_promises = [];
 const url = 'mongodb://localhost:27017/' + args.database;
@@ -24,22 +26,23 @@ const db = monk(url)
                 } );
 
                 Promise.all(ocid_promises).then( results => {
+                    db.close();
                     let uniques = {};
                     results.map( (result) => {
                         result.map( (line) => {
-                            if(!uniques.hasOwnProperty(line.ocid)) uniques[line.ocid] = 1;
+                            if(!uniques.hasOwnProperty(line[idField])) uniques[line[idField]] = 1;
                         } );
                         delete result;
                     } );
                     delete results;
 
-                    Object.keys(uniques).map( (line) => {
-                        console.log(line);
-                        delete uniques[line];
-                    } );
-
-                    db.close();
-                    process.exit();
+                    // Object.keys(uniques).map( (line) => {
+                    //     console.log(line);
+                    //     delete uniques[line];
+                    // } );
+                    process.stdout.write( Object.keys(uniques).join('\n') + '\n', process.exit );
+                    // console.log('done');
+                    // process.exit();
                 });
             });
 
@@ -50,14 +53,14 @@ function findAllOCIDs(db, collection) {
             {
                 "$group" : {
                     "_id" : {
-                        "ocid" : "$ocid"
+                        [idField] : "$" + idField
                     }
                 }
             },
             {
                 "$project" : {
                     "_id" : 0,
-                    "ocid" : "$_id.ocid"
+                    [idField] : "$_id." + idField
                 }
             }
         ]
